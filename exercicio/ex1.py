099aeaf376f7e09ce05f5a4073eeb131d9506933
Thales Sollace <maybeviick@gmail.com>
2025-04-11 03:58:56 -0300

    img + f-string

---
+++ exercicio/ex1.py
@@ -0,0 +1,12 @@
+nome = "Red 'Quinn'"
+sobrenome = "Hearts"
+idade = 19
+ano_nasc = 2005
+maior_idade = idade >= 18
+altura_metros = 1.63
+
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade)
+print("É maior de idade?", maior_idade)
+print("Altra:", altura_metros)
---
+++ exercicio/ex2.py
@@ -0,0 +1,12 @@
+# IMC
+
+nome = "Chad Charming"
+altura = 1.74
+peso = 74
+imc = peso / (altura * altura)
+
+print(f'{nome} tem {altura:.2f} de altura, \n pesa {peso} quilos e seu imc é \n {imc:.2f}')
+
+#f-string
+#formatação de string
+#serve pra chamar variavel dentro da str
