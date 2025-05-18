7d5949d14a9c95fdfaa284c17767b2039345278b
Thales Sollace <maybeviick@gmail.com>
2025-05-18 19:19:34 -0300

    for function

---
+++ exercicio/for.py
@@ -0,0 +1,9 @@
+#coisinhas de nerdola da disney pq eu posso <3
+
+#reinos = ["Wonderland", "Cinderellasburg", "Atlantis", "Corona"]
+reinos2 = [["Wonderland","Red Hearts"], ["Cinderellasburg","Chloe Charming"], ["Atlantis","Maeryn Baitola"], ["Corona","Ruby Rider"]]
+
+#print(reinos2[0][1]) #SELOKO EU SOU MT GENIO 
+
+for i in range(len(reinos2)): #DESCOBRI HAHAHAHAHAHHHAHAHHAA
+   print(f"Reino: {reinos2[i][0]} \nRepresentante: {reinos2[i][1]} \n")
