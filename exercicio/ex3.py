37cf4d956a8bae09cd85215d5b5b9b037a71e141
Thales Sollace <maybeviick@gmail.com>
2025-05-18 19:18:57 -0300

    format

--- exercicio/ex3.py
+++ exercicio/ex3.py
@@ -4,15 +4,4 @@ c = 1.1
 
 formato = "a={} b={} c={:.2f}".format(a, b, c)
 
-#print(formato)
-
-# mais coisinhas
-
-#reinos = ["Wonderland", "Cinderellasburg", "Atlantis", "Corona"]
-reinos2 = [["Wonderland","Red Hearts"], ["Cinderellasburg","Chloe Charming"], ["Atlantis","Maeryn Baitola"], ["Corona","Ruby Rider"]]
-
-print(reinos2[0][1]) #SELOKO EU SOU MT GENIO 
-
-#for(i=0; i<len(reinos2); i++){ #como q o for funciona nisso meu deus??????????
-#   print(f'"Reino: {reinos2[i]} \n Representante: {reinos2[i][1]}")
-#}
+print(formato)
