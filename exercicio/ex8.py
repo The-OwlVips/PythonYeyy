5bc0c2042e5c029c6857d2839097fdd8e21285a3
Thales Sollace <maybeviick@gmail.com>
2025-06-06 03:14:59 -0300

    in and not in

---
+++ exercicio/ex8.py
@@ -0,0 +1,20 @@
+#in e not in
+#  0  1  2  3  4  5
+#  S  T  R  I  N  G
+# -6 -5 -4 -3 -2 -1
+
+nome = "Chloe"
+#print('oe' in nome)
+
+if 'oe' in nome:
+    for i in range(len(nome)):
+        print(f"{nome[i]}")
+#YAUTGYUSHFIAH WTF IT WORKS
+
+print('-'*10)
+
+sobrenome = "Charming"
+
+if "Hearts" not in sobrenome:
+    for i in range(len(sobrenome)):
+        print(f"{sobrenome[i]}") 
