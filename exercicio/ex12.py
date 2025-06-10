d98fb767fc2adfe74f6fe9dd457bc3ae1505bcac
Thales Sollace <maybeviick@gmail.com>
2025-06-10 03:44:00 -0300

    is and is not

---
+++ exercicio/ex12.py
@@ -0,0 +1,22 @@
+#is ou not is
+#None
+
+CANON = True
+glassheart = True
+print(glassheart is CANON)
+
+#EU TENHO O PODER NAS MINHAS MÃOS AHAHAHHAHAHAHAHA
+
+print("-"*30)
+
+""" 
+    Faça um programa que peça ao usuário para digitar um número inteiro, informe se esse número é par ou ímpar. Caso o usuário não digite um número inteiro, informe que não é um número inteiro
+"""
+
+"""
+    Faça um programa que pergunte a hora ao usuário e, com baseando-se no horário descrito, exiba a saudação apropriada. Ex: Bom Dia 0-11, Boa Tarde 12-17, Boa Noite 18-23
+"""
+
+"""
+    Faça um programa que peça o nome do usuário. Se o nome tiver 4 letras ou menos escreva "Seu nome é curto"; Se tiver 5-6 letras, escreva "Seu nome é normal"; Maior que 6 escreva "Seu nome é grande". 
+"""
